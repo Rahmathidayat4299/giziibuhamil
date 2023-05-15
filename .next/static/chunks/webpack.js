55c10f0fc36f782489429329c42cfc336b92aa8f
@hrahmat950 <hrahmat950@gmail.com>
2023-05-15 17:37:21 +0700

    next custom home and navbar mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "21ab00445aafddef"; }
+/******/ 		__webpack_require__.h = function() { return "3da2d6b193dd1971"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
@@ -36,6 +37,7 @@ const Navbar = () => {
             ></path>
           </svg>
         </button>
+
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
