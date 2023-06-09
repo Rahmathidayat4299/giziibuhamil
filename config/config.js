6fe7f150f35c41367afdf997a2a7bd844796d13f
thelastman112 <rizkymaestro12@gmail.com>
2023-06-09 21:25:52 +0700

    change all to conf without env

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ import { initializeApp, getApps } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	apiKey: "AIzaSyCRl6Cy_J20_0Hyn8cUupKPcfdC0X1LfNM",
 	authDomain: "giziibuhamil-83b72.firebaseapp.com",
 	projectId: "giziibuhamil-83b72",
 	databaseURL: "https://giziibuhamil-83b72-default-rtdb.asia-southeast1.firebasedatabase.app",
