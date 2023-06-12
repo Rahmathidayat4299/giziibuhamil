eaa7cd058f833370a5f3b2c51825bbec6843722a
thelastman112 <rizkymaestro12@gmail.com>
2023-06-12 14:10:04 +0700

    add some changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "65c8c4209983ef7f"; }
+/******/ 		__webpack_require__.h = function() { return "5e926af3e6646937"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
